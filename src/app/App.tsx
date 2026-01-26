8ce5cdcb92a577ec868f4d3761e637e9974c0722
Praveen Joshi <joshipraveen535@gmail.com>
2026-01-26 22:33:58 +0530

    chore: clean working RN setup with src structure

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Expense Control App 🚀</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
+
+export default App;
